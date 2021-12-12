b18e92ca87e1b19a6d0a7f96b079031f5072fe67
Romain Roux <r.roux03@hotmail.fr>
2021-12-12 17:38:28 +0100

    mainc 2

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 int main(){
-	for(int i = 1;i<=10;i++){
-		printf("%d\n",i);
+	srand(time(NULL));
+	printf("I have in mind a number in between 1 and 100, can you find it?\n");
+	int n = rand() % 100;
+	int e = 0;
+	scanf("%d",&e);
+	while(n != e){
+		if(e < n){
+			printf("The number to guess is higher\n");
+			scanf("%d",&e);
+		}
+		else if(e > n){
+			printf("The number to guess is lower\n");
+			scanf("%d",&e);
+		}
 	}
+	printf("You just found the number, it was indeed %d\n", n);
 	return 0;
 }
